b942c8c5641709adec7af9ed0d3fa2eda36094db
omnia atef <oatef@activedd.com>
2020-11-30 14:01:28 +0200

    updating careers

--- build/js/common.js
+++ build/js/common.js
@@ -1,3 +1,30 @@
 function goToContactUs() {
   window.location.href = "/contactUs.html";
 }
+
+// $('.applyButton').on('click', function(event){
+//   console.log('event', event.currentTarget.id);
+  
+//   if(event.currentTarget.id == "applyUI"){
+//     window.location.href = "/Active%20Work/Add%20Pluses%20(new%20website)/Add-Pluses/src/templates/contactUs.html";
+//     console.log('position', $(".positionForm"));
+//     selectPosition();
+//   }
+//   else if(event.currentTarget.id == "applyAccountManager"){
+//     document.getElementById("accountManag").selected = "true";
+//   }
+//   else{
+//     document.getElementById("backendDev").selected = "true";
+//   }
+// });
+
+// function selectPosition(){
+//   document.addEventListener("DOMContentLoaded", function() { 
+//     console.log('hello');
+//     document.getElementById("applyingPostion").selected = "true";
+//     $(".positionForm").css('display', 'block');
+//     $(".projectForm").css('display', 'none');
+//     $(".otherForm").css('display', 'none');
+//     document.getElementById("uiDev").selected = "true";
+// });
+// }
