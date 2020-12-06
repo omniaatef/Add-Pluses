392539ae6bb4d13eb7149630dce163010f6d1d26
omnia atef <omnia.atef@add.int>
2020-12-06 13:48:46 +0200

    updating contact us form

--- build/js/common.js
+++ build/js/common.js
@@ -22,38 +22,24 @@ function getCurrentYear(){
   var year = date.getFullYear();
   addPlusesYear.innerHTML = "© "+year+ " Add pluses";
 }
-
 getCurrentYear();
-// var applySelected = "";
-// $('.applyButton').on('click', function(event){  
-//   if(event.currentTarget.id == "applyUI"){
-//     applySelected = "UI";
-//   }
-//   else if(event.currentTarget.id == "applyAccountManager"){
-//     applySelected = "QC";
-//   }
-//   else{
-//     applySelected = "BE";
-//   }
-//   selectPosition(applySelected);
-// });
+
+var applySelected = "";
+$('.applyButton').on('click', function(event){  
+  if(event.currentTarget.id == "applyUI"){
+    applySelected = "UI";
+  }
+  else if(event.currentTarget.id == "applyAccountManager"){
+    applySelected = "QC";
+  }
+  else{
+    applySelected = "BE";
+  }
+  selectPosition(applySelected);
+});
 
 
-// function selectPosition(pos){
-//     window.location.href = "/contactUs.html";
-//     if(pos == "UI"){
-//       document.getElementById("applyingPostion").selected = "true";
-//       $(".positionForm").css('display', 'block');
-//       $(".projectForm").css('display', 'none');
-//       $(".otherForm").css('display', 'none');
-//       document.getElementById("uiDev").selected = "true";
-//     }
-//     else if(pos == "QC"){
-//       console.log('select', pos);
+function selectPosition(pos){
+    window.location.href = "/contactUs.html?position="+pos;
+}
 
-//       document.getElementById("accountManag").selected = "true";
-//     }
-//     else{
-//       document.getElementById("backendDev").selected = "true";
-//     }
-// }
